d55dd40d6aee258e39d39c477acffe24a34e14b4
Philip Miller <philip@philiplmiller.com>
2025-09-18 07:11:50 -0400

    Version 0.0.2 Added Meta Data for shipping class

--- ampm_order_split.php
+++ ampm_order_split.php
@@ -4,12 +4,13 @@
 Name: AMPM Order Split
 Plugin Name: AMPM Order Split
 Plugin URI: https://ampmllc.co
-Description: Release 0.0.1
+Description: Release 0.0.2
 Author: AMPM LLC
-Version: 0.0.1
+Version: 0.0.2
 Author URI: https://ampmllc.co
 Version History:
 * Version 0.0.1 Baseline
+* Version 0.0.2 Added Meta Data for shipping class
 */
 
 defined( 'ABSPATH' ) || exit; // block direct access to plugin PHP files by adding this line at the top of each of them
@@ -32,25 +33,29 @@ function AMPM_split_order_after_checkout( $order_id ) {
     $order = wc_get_order( $order_id );
     if ( ! $order || $order->get_meta( '_order_split' ) ) return;
     $items_by_shipping_class = array();
- 
+    $shipping_class_array = array();
+
     foreach ( $order->get_items() as $item_id => $item ) {
         $product = $item->get_product();     
         $class_id = $product->get_shipping_class_id();
+        $shipping_class_array[$class_id] = get_shipping_class_name($class_id);
         $items_by_shipping_class[$class_id][$item_id] = $item;
     }
- 
+
+    $values = array_values($shipping_class_array);
+    $orig_ship_class = $values[0];
+    
     if ( count( $items_by_shipping_class ) > 1 ) {
       foreach ( array_slice( $items_by_shipping_class, 1 ) as $class_id => $items ) {
          $args = array(
             'status'      => 'pending', // Or 'processing', 'completed', etc.
             'customer_id' => $order->get_customer_id(), // Optional: Assign to a specific customer
-            'customer_note' => 'This Order is split from order: '.$order_id.' for processing by BVC.',
+            'customer_note' => 'This Order is split from order: '.$order_id.' to assist in processing by Blue Valley Cabinets.'.' for Class ID: '.$class_id,
          );
          $new_order = wc_create_order( $args );
-         //$new_order = wc_create_order();
          $new_order->set_address( $order->get_address( 'billing' ), 'billing' );
          if ( $order->needs_shipping_address() ) $new_order->set_address( $order->get_address( 'shipping' ) ?? $order->get_address( 'billing' ), 'shipping' );
-          
+
          foreach ( $items as $item_id => $item ) {
             $new_item = new WC_Order_Item_Product();
             $new_item->set_product( $item->get_product() );
@@ -65,16 +70,17 @@ function AMPM_split_order_after_checkout( $order_id ) {
             $new_order->add_item( $new_item );
             $order->remove_item( $item_id );
          }
-         //$new_order->set_customer_id( $$order->get_customer_id() );
-         $new_order = copy_meta($order,$new_order);        
-         $new_order->add_order_note( 'Split from order ' . $order_id );
-         $new_order->calculate_totals();        
+         $new_order = copy_meta($order,$new_order);
+         $new_order->update_meta_data('_shipping_class',$values[1], true);      
+
+         $new_order->calculate_totals();  
          $new_order->set_payment_method( $order->get_payment_method() );
          $new_order->set_payment_method_title( $order->get_payment_method_title() );         
          $new_order->update_status( $order->get_status() );
- 
+         
          $order->calculate_totals();
          $order->update_meta_data( '_order_split', true );
+         $order->update_meta_data( '_shipping_class', $orig_ship_class, true);
          $order->save();
       }
  
@@ -92,4 +98,17 @@ function copy_meta($order,$new_order)
    return $new_order;
 }
 
+function get_shipping_class_name($shipping_class_id)
+{
+   if ( $shipping_class_id > 0 ) { // Check if a shipping class is assigned
+      $shipping_class_term = get_term( $shipping_class_id, 'product_shipping_class' );
+
+      if ( ! is_wp_error( $shipping_class_term ) && is_a( $shipping_class_term, 'WP_Term' ) ) {
+         $shipping_class_name = $shipping_class_term->name;
+         return $shipping_class_name;
+      }
+   } else {
+      return 'No shipping class assigned.';
+   }
+}
 ?>
